83d97ff91eca9b2dd32ea4147a57db57509cd25f
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-19 17:59:16 +0900

    movies コントローラに処理を記述

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,20 @@
 class MoviesController < ApplicationController
+  require "dotenv/load"
+  require "google/apis/youtube_v3"
+
+  def find_videos(keyword)
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = ENV["GOOGLE_API_KEY"]
+    option = {
+      q: keyword,
+      type: "video",
+      max_results: 10,
+      order: :date,
+    }
+    youtube.list_searches(:id, :snippet, option)
+  end
+
   def index
+    @youtube_data = find_videos("ポケモンカード")
   end
 end
